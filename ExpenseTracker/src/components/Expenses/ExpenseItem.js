770f15cacdd72bc8f63f96686f8ad9926f3ba474
Priyanka <priyanka11g0@gmail.com>
2021-08-27 10:56:12 +0530

    first commit

---
+++ ExpenseTracker/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import classes from './ExpenseItem.module.css';
+
+const ExpenseItem = (props) => {
+  return (
+    <li>
+    <Card className={classes['expense-item']}>
+      <ExpenseDate date={props.date} />
+      <div className={classes['expense-item__description']}>
+        <h2>{props.title}</h2>
+        <div className={classes['expense-item__price']}>${props.amount}</div>
+      </div>
+    </Card>
+    </li>
+  );
+}
+
+export default ExpenseItem;
